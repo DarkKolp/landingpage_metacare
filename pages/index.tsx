089cc8e47159433828b8ac79467ff2f1d2195627
DarkKolp <nunoandresilva199@gmail.com>
2023-06-18 15:03:43 +0100

    Add existing file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Inter } from 'next/font/google'
+import Head from 'next/head' 
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,79 +9,99 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
   return (
-    <main>
-      <nav className="w-full h-[150px] 
-      bg-[#D6E9F8]
-      shadow
-      flex items-center
-      ">
-        <div className='pl-[5%]'>
-          <img src="/metacareLogo.png" alt="Metacare Logo" width={180} height={180}/>
-        </div>
-        <div className='w-full flex justify-end'>
-          <a href="https://www.crotaluslabs.com/" 
-          className='text-gray-600
-          pr-[15%]
-          text-xl'>
-            About Us
-          </a>
-        </div>
-      </nav>
       
-      <div className='w-full flex flex-row mb-[50px]'>
+      <main>
+        <Head>
+          <title>Metacare Health</title>
+          <link rel="shortcut icon" href="/public/favicon.ico" />
+        </Head>
+        <nav className="w-full h-[120px] 
         
-        <div className='w-full flex flex-col mt-[120px] pl-[5%]'>
-            <h2 className='text-6xl text-[#091562]'>
-                    A Blockchain-based Healthcare <br></br>Data Management Solution.
-            </h2>
-            
-            <h2 className='text-2xl text-[#091562]
-            mt-[40px]'>
-            <span className="type-info"></span>
-            </h2>
-
-            <div className="flex justify-start">
-              <button type="button"
-              className="h-[50px] w-[200px] mt-[50px] rounded-lg
-              text-xl text-white font-sans
-              bg-gradient-to-r from-[#2F80ED] to-[#B2FFDA] 
-              shadow-lg shadow-gray-400">
-                Fill the form
+        flex items-center
+        ">
+          <div className='pl-[2%] pt-[1%]'>
+            <img src="/metacareLogo.png" alt="Metacare Logo" width={150} height={150}/>
+          </div>
+          <div className='w-full flex justify-end items-center'>
+            <a href="https://www.crotaluslabs.com/" 
+            className='text-[#0093ff]
+            pr-[3%]
+            text-xl font-bold'>
+              Documentation
+            </a>
+            <a href="https://www.crotaluslabs.com/" 
+            className='text-[#0093ff]
+            pr-[3%]
+            text-xl font-bold'>
+              About Us
+            </a>
+            <a href="https://www.crotaluslabs.com/" 
+            className='text-[#0093ff]
+            pr-[3%]
+            text-xl font-bold'>
+              Blog
+            </a>
+            <div className="
+            pr-[3%]">
+              <button className="h-[50px] w-[130px] rounded-lg
+                  text-l text-white font-sans
+                  bg-[#0093ff]">
+                    Contact
               </button>
             </div>
-
-            <div className="flex justify-center mt-[80px] text-xl text-[#091562]">
-              <h2>
-                We're trusted by the best in business
-              </h2>
-            </div>
-
-            <div className="flex flex-row justify-center mt-[50px]">
-              <div>
-                <img src="/asus-logo.png" alt="" width={150}/>
+          </div>
+        </nav>
+        <div className="w-full h-[3px] bg-[#091562]"></div>
+        
+        <div className='w-full flex flex-row mb-[50px]'>
+          
+          <div className='w-3/5 flex flex-col mt-[50px] pl-[5%]'>
+              <div className="w-full ml-[15%]">
+                <img src="/metacareLogo.png" alt="Metacare Logo" width={250} height={250}/>
               </div>
-            </div>
+              <h2 className='text-5xl text-[#091562] font-bold mt-[40px]'>
+                      Your Personal Pathway to<br></br>your Healtier Future.
+              </h2>
+              
+              <h2 className='text-2xl text-[#091562] font-bold
+              mt-[70px]'>
+              Unleash the power of your personal Data Digital Twin.
+              </h2>
+              <p className=" text-[#091562] text-xl
+              mt-[20px]">Join our waitlist and be the first to experience a new era of healthcare</p>
 
+              <div className="flex justify-start ml-[30px] mt-[70px]">
+                <input type="email" placeholder="Enter your email"
+                className="h-[50px] w-[300px] rounded-lg border border-gray-400/75 mr-5 pl-4" />
+                <button type="button"
+                className="h-[50px] w-[200px] rounded-lg
+                text-xl text-white font-sans
+                bg-[#0093ff] 
+                shadow-lg shadow-gray-400">
+                  Submit
+                </button>
+              </div>
+
+              
+
+          </div>
+
+          <div className='fixed  top-[123px] right-0'>
+            <img src="/bgimg.png" alt="Blue man with open arms" width={643} height={857}/>
+          </div>
         </div>
 
-        <div className='w-2/3 h-fit flex justify-end mt-[50px] pr-[10%]'>
-          <img src="/blue-men.jpg" alt="Blue man with open arms" width={321} height={573}/>
-        </div>
-      </div>
 
-
+        
+        <footer className="h-[100px] flex flex-col justify-center items-center">
+          <a href="https://www.crotaluslabs.com/" target="_blank" rel="noopener noreferrer"
+          className="text-[#0093ff] font-bold 
+          ">
+            Powered by Crotalus Labs&reg;
+          </a>
+        </footer>
       
-      <footer className="h-[100px] flex flex-col justify-center items-center
-      border-t-[2px] border-t-[#eaeaea]">
-				<a href="https://www.crotaluslabs.com/" target="_blank" rel="noopener noreferrer"
-        className="text-[#0093ff] font-bold 
-        ">
-					Powered by Crotalus Labs&reg;
-				</a>
-			</footer>
-    
-    </main>
-
+      </main>
     
   )
 }
